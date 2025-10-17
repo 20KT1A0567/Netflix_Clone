4dba744e6fb883b8f0ede9946df4e80e7c865659
20KT1A0567 <venkataraodama660@gmail.com>
2025-10-17 21:43:23 +0530

    hello1

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-import { generateTokenAndSetCookie } from "../utils/generateToken.js";
+import { generateTokenAndSetCookie } from "../utils/generatetoken.js";
+
 
 
 
